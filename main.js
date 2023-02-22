9f730a462fbcd1df0c8235e8bfe82b9f3d90ff6a
Elvis Lopz <elvis@huemanstudio.com>
2023-02-21 20:24:05 -0800

    Commit Next Book

---
+++ main.js
@@ -0,0 +1,24 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+let quote = [
+    'The Dress Maker of Auschwitz by Lucy Adlington',
+    'The Spanish Love Deception by Elena Arma',
+    'The Deal by Elle Kennedy',
+    'Just Like Home by Sarah Gailey'
+];
+
+btn.addEventListener('click', function(){
+    var randomQuote = quote[Math.floor(Math.random() * quote.length)]
+    output.innerHTML = randomQuote;
+});
+
+let popup =  document.getElementById('popup');
+
+function openPopup() {
+    popup.classList.add('open-popup');
+}
+
+function closePopup() {
+    popup.classList.remove('open-popup');
+    popup.classList.add('close-popup');
+}
